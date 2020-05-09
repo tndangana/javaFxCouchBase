479a00286c152ef39cee0a450dc8eb3c51d822dd
Tonderai Ndangana <ndangana8@gmail.com>
2020-05-09 13:04:30 +0200

    set up service

--- src/main/java/zw/co/abn/covid/service/IGenericService.java
+++ src/main/java/zw/co/abn/covid/service/IGenericService.java
@@ -1,4 +1,18 @@
 package zw.co.abn.covid.service;
 
-public interface IGenericService {
+import java.util.List;
+import java.util.Map;
+
+//interface with all available methods which will be used to interact with couch base.These methods are implemented in the genericService class
+public interface IGenericService<Type,T> {
+
+    List<T> findAll(String viewName);//get all objects
+    boolean save(Map<Type,T> properties,String viewName); //save properties of an object through the use of a HashMap
+    boolean update(Map<Type,T> properties,String id);  //update similar to save handler.Saves updated object
+    boolean deleteById(String viewName,String id); //deletes object through the use of an Id
+    boolean recordExist(String viewName,String id); //checks if a record exists
+     Map<String ,Object> findById(String viewName,String id);  //finds an object through the use of an id
+
+
+
 }
--- src/main/java/zw/co/abn/covid/service/impl/GenericService.java
+++ src/main/java/zw/co/abn/covid/service/impl/GenericService.java
@@ -1,4 +1,108 @@
 package zw.co.abn.covid.service.impl;
 
-public class GenericService {
+import com.couchbase.lite.*;
+import org.springframework.stereotype.Service;
+import zw.co.abn.covid.configuration.DatabaseConfiguration;
+import zw.co.abn.covid.service.IGenericService;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+//Service class which implements and brings life to the generic service interface
+public class GenericService implements IGenericService<String, Object> {
+
+     //Couch base configurations are setup in here
+    private final DatabaseConfiguration databaseConfiguration;
+    private final Database database;
+    private Document document = null;
+
+    //instantiate configuration and database
+    public GenericService() {
+        this.databaseConfiguration = DatabaseConfiguration.getInstance();
+        this.database = this.databaseConfiguration.getDatabase();
+    }
+
+
+    @Override
+    public List<Object> findAll(String viewName) {
+        List<Object> list = new ArrayList<>();
+        try {
+            View view = database.getView(viewName);
+            view.setMap((document, emitter) -> emitter.emit(document.get("_id"), document), "1");
+            Query query = view.createQuery();
+            QueryEnumerator result = query.run();
+            result.forEach(queryRow -> {
+                document = queryRow.getDocument();
+                list.add(document.getProperties());
+            });
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        } finally {
+            document = null;
+        }
+        return list;
+    }
+
+    @Override
+    public boolean save(Map<String, Object> properties, String viewName) {
+        properties.put("type", viewName);
+        properties.putAll(properties);
+        Document document = database.createDocument();
+        try {
+            document.putProperties(properties);
+            return true;
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean update(Map<String, Object> properties, String id) {
+        Document doc = database.getDocument(id);
+        Map<String, Object> modelProperties = new HashMap<String, Object>();
+        modelProperties.putAll(doc.getProperties());
+        modelProperties.putAll(properties);
+        try {
+            doc.putProperties(modelProperties);
+            return true;
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean deleteById(String viewName, String id) {
+        Document task = database.getDocument(id);
+
+        try {
+            if (!task.getProperties().isEmpty()) {
+                task.delete();
+                return true;
+            }
+        } catch (CouchbaseLiteException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean recordExist(String viewName, String id) {
+        Document document = database.getDocument(id);
+        if (!document.getProperties().isEmpty())
+            return true;
+        return false;
+    }
+
+    @Override
+    public Map<String,Object> findById(String viewName, String id) {
+         Document document = database.getDocument(id);
+        return document.getProperties();
+    }
+
+
 }
